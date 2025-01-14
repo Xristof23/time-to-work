53a507b30a42d6f403a6d6fe5a1dcba980054bb7
Christof Röhl <152093131+Xristof23@users.noreply.github.com>
2025-01-14 09:59:45 +0100

    MainTiming works, add properTimeformatter (#1)

---
+++ utils.js
@@ -0,0 +1,32 @@
+function properTimeFormatter(timeInMs, options) {
+  const allSeconds = Math.round(timeInMs / 1000);
+  const rawSeconds = Math.floor(allSeconds % 60);
+  const seconds = rawSeconds.toLocaleString("en-US", {
+    minimumIntegerDigits: allSeconds < 60 && rawSeconds < 10 ? 1 : 2,
+    useGrouping: false,
+  });
+
+  const rawHours = allSeconds < 3600 ? 0 : Math.floor(allSeconds / 3600);
+  const hours = rawHours.toLocaleString("en-US", {
+    minimumIntegerDigits: rawHours < 10 ? 1 : 2,
+    useGrouping: false,
+  });
+
+  const rawMinutes =
+    allSeconds < 60 ? 0 : Math.floor((allSeconds - rawHours * 3600) / 60);
+  const minutes = rawMinutes.toLocaleString("en-US", {
+    minimumIntegerDigits: allSeconds < 3600 && rawMinutes < 10 ? 1 : 2,
+    useGrouping: false,
+  });
+
+  const unit = allSeconds < 60 ? "sec" : allSeconds < 3600 ? "min" : "hours";
+  const formattedTime =
+    allSeconds < 60
+      ? `${seconds} ${unit}`
+      : allSeconds < 3600
+      ? `${minutes}:${seconds} ${unit}`
+      : `${hours}:${minutes}:${seconds} ${unit}`;
+  return formattedTime;
+}
+
+export { properTimeFormatter };
