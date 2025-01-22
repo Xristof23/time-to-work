55f1955260cbbdc3f518b189fcc9d8ba656b8ba5
Christof <userpattern@gmail.com>
2025-01-22 23:35:13 +0100

    entryDetails working, need styling

--- components/EntryDetails/EntryDetails.js
+++ components/EntryDetails/EntryDetails.js
@@ -1,4 +1,15 @@
-export default function EntryDetails() {
+export default function EntryDetails(element) {
   const entryDetails = document.createElement("div");
+  entryDetails.setAttribute("id", `${element.id}d`);
+  entryDetails.innerHTML = /* html */ `
+
+
+<p class="detail_paragraph">Project: <br/>${element.project} </p>
+<p class="detail_paragraph">Category: ${element.category} </p>
+<br/>
+<p class="detail_paragraph">Time spent: ${element.timeSpent}</p>
+<p class="detail_paragraph">Id: ${element.id} </p>
+  
+  `;
   return entryDetails;
 }
--- components/ListEntry/ListEntry.js
+++ components/ListEntry/ListEntry.js
@@ -1,29 +1,38 @@
 import { wantedDelete } from "../../modalContent.js";
+import EntryDetails from "../EntryDetails/EntryDetails.js";
 import Modal from "../Modal/Modal.js";
+let counter = 0;
 
 export default function ListEntry(element) {
   const listEntry = document.createElement("article");
   listEntry.classList.add("listEntry");
   listEntry.setAttribute("id", `${element.id}`);
   listEntry.innerHTML = /* html */ `
-
   <div>
-<h3>Entry from ${element.date}, ${element.time}</h3><button class = "delete_button" data-js="detail-delete-button">X</button></div>
-<p class="detail_paragraph">Project: ${element.project} </p>
-<p class="detail_paragraph">Task: ${element.task} </p>
-<p class="detail_paragraph">Category: ${element.category} </p>
-<p class="detail_paragraph">Time spent: ${element.timeSpent}</p>
-<p class="detail_paragraph">Id: ${element.id} </p>
-  
+  <h3>Entry from ${element.date}, ${element.time}</h3><button class = "delete_button" data-js="detail-delete-button">X</button>
+  </div>
+  <p class="detail_paragraph">Task: ${element.task} </p>
+  <button data-js="detail-button">more details</button>
   `;
+
   const deleteButton = listEntry.querySelector(
     '[data-js="detail-delete-button"]'
   );
   deleteButton.addEventListener("click", handleAreYouSure);
+  const detailButton = listEntry.querySelector('[data-js="detail-button"]');
+  detailButton.addEventListener("click", handleShowDetails);
 
   function handleAreYouSure() {
     const id = element.id;
     listEntry.append(Modal(wantedDelete, id));
+  }
+
+  function handleShowDetails() {
+    const entryDetails = document.getElementById(`${element.id}d`);
+    counter % 2 === 0
+      ? listEntry.append(EntryDetails(element))
+      : entryDetails.remove();
+    ++counter;
   }
 
   return listEntry;
