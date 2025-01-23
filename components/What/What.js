f526488e71a2d7bf0988828d0df956200b7a4ee6
Christof <userpattern@gmail.com>
2025-01-23 16:53:35 +0100

    modal bugfix

--- components/What/What.js
+++ components/What/What.js
@@ -4,7 +4,9 @@ export default function What() {
   what.setAttribute("id", "what");
   what.innerHTML = /*html*/ `
     <h2>What is this?<h2>
-    <p class="standard_text"> It's a tool to control time. Like a time machine. Nah. </p>
+    <p class="standard_text"> It's a tool to control time. Like a time machine. Nah. More like a stop watch that records the time you spend on a task. </p>
+    <p class="standard_text"> More functions on the way. </p>
     `;
+
   return what;
 }
