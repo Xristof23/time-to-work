e36b017d25e135072437369d8a994280822f20d8
Christof <userpattern@gmail.com>
2025-01-30 10:20:18 +0100

    set new entry as starting point again

--- components/App/App.js
+++ components/App/App.js
@@ -1,6 +1,5 @@
 import Footer from "../Footer/Footer.js";
 import Header from "../Header/Header.js";
-import ListContainer from "../ListContainer/ListContainer.js";
 import MainTiming from "../MainTiming/MainTiming.js";
 
 const userEntries = JSON.parse(localStorage.getItem("RecordedTasks")) || [];
@@ -10,8 +9,7 @@ export default function App() {
   app.setAttribute("id", "app");
   app.classList.add("app");
   app.append(Header());
-  app.append(ListContainer(userEntries));
-  // app.append(MainTiming());
+  app.append(MainTiming());
   app.append(Footer());
   return app;
 }
