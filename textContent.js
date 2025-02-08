faf3a7b211c9052037f1841021469fe131ee4727
Christof <userpattern@gmail.com>
2025-02-08 10:39:46 +0100

    quick text content fix

--- textContent.js
+++ textContent.js
@@ -2,7 +2,7 @@ const whatContent = {
   headline: "What is this?",
   id: "what",
   paragraph1:
-    "It's a tool to control time. Like a time machine. Nah. More like a stop watch that records the time you spend on a task. But has a cool analog style clock!",
+    "It's a tool to control time. Like a time machine. Nah. More like a stop watch that records the time you spend on a task. But there's a cool analog style clock!",
   paragraph2: "More functions on the way.",
 };
 
@@ -10,7 +10,7 @@ const noTasksContent = {
   headline: "No tasks",
   id: "noTasks",
   paragraph1:
-    "No saved tasks found. Press the &quot;new task&quot; button to start one or use &quot;demo mode&quot; to generate examplary data!",
+    "No saved tasks found. Press the &quot;new task&quot; button to start one or use &quot;demo mode&quot; to generate exemplary data!",
 };
 
 export { noTasksContent, whatContent };
